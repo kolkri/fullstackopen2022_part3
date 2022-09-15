8690ca364f686f7fe0f95d64d5b8b0b6c4f69421
kolkri <kristiinakolu@gmail.com>
2022-09-15 16:31:41 +0300

    part 3a done

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+morgan.token('body', req => {
+    return JSON.stringify(req.body)
+  })
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+      },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234245"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+      }
+  ]
+
+
+//   app.get('/', (req, res) => {
+//     res.send('<h1>Hello World!</h1>')
+//   })
+  
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+  app.get('/info', (req, res) => {
+    const amount = persons.length
+    const currentdate = new Date()
+    res.send(`<p>Phonebook has info for ${amount} people</p>
+    <p>${currentdate}</p>`)
+  
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+  
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+  })
+  
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+  
+    response.status(204).end()
+  })
+
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+    if (!body.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+      }
+    if(persons.some(person => person.name === body.name)){
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+          })
+    }
+  
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
