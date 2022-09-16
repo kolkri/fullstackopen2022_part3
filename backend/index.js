79a9f4c51872add08f35dae9bdaddac77d88c0db
kolkri <kristiinakolu@gmail.com>
2022-09-16 16:36:48 +0300

    part 3c done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -9,70 +11,47 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 
 
-
 morgan.token('body', req => {
     return JSON.stringify(req.body)
   })
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-      },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234245"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-      }
-  ]
+
 
   app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(result => {
+      res.json(result)
+    })
   })
 
   app.get('/info', (req, res) => {
-    const amount = persons.length
+    const amount = Person.length
     const currentdate = new Date()
     res.send(`<p>Phonebook has info for ${amount} people</p>
     <p>${currentdate}</p>`)
   
   })
 
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-  
-    if (person) {
+  app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+      if (person){
         response.json(person)
-    } else {
+      } else {
         response.status(404).end()
-    }
+      }
+    })
+    .catch(error => next(error))
   })
   
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-  
-    response.status(204).end()
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
   })
 
-  const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
+ 
+
   
   app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -87,24 +66,51 @@ let persons = [
           error: 'number missing' 
         })
       }
-    if(persons.some(person => person.name === body.name)){
-        return response.status(400).json({ 
-            error: 'name must be unique' 
-          })
-    }
+    // if(Person.some(person => person.name === body.name)){
+    //     return response.status(400).json({ 
+    //         error: 'name must be unique' 
+    //       })
+    // }
+  
+    const person = new Person ({
+      name: body.name,
+      number: body.number
+    })
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+  })
+
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
   
     const person = {
       name: body.name,
-      number: body.number,
-      id: generateId(),
+      number: body.number
     }
   
-    persons = persons.concat(person)
-  
-    response.json(person)
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
   })
   
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT 
     app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  
+
+  app.use(errorHandler)
